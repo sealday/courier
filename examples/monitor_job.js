77c5b609d681028fbed62b1bb1d11ec7a6278211
sealday <sealday@gmail.com>
2017-03-03 17:32:47 +0800

    对监视器加锁

--- examples/monitor_job.js
+++ examples/monitor_job.js
@@ -1,6 +1,8 @@
 'use strict';
 const redis = require('redis');
 const EventEmitter = require('events');
+const Warlock = require('node-redis-warlock');
+const async = require('async');
 
 const events = new EventEmitter();
 // 在监视的里面，需要监视两个队列，分别是 delayed 队列和 active 队列
@@ -8,37 +10,56 @@ const client = redis.createClient({
   prefix: 'courier:'
 });
 
-const pubsubClient = client.duplicate();
+// 我们应该只有一个监视器
+const warlock = Warlock(client);
 
-const promote = () => client.lrange('jobs:delayed', 0, -1, (err, jobs) => {
-  jobs.forEach(job => {
-    client.hgetall(`job:${job}`, (err, job) => {
-      if (job.createdAt * 1 + job.delay * 1 <= Date.now()) {
-        const multi = client.multi();
-        multi.hmset(`job:${job}`,
-          'promotedAt', Date.now(),
-          'updatedAt', Date.now(),
-          'state', 'ready'
-        );
-        multi.lrem('jobs:delayed', 1, job.id);
-        multi.lrem(`jobs:${job.type}:delayed`, 1, job.id);
-        multi.lpush('jobs:ready', job.id);
-        multi.lpush(`jobs:${job.type}:ready`, job.id);
-        events.emit('promotion');
-        multi.exec(() => {
-          client.publish(`events:${job.id}`, JSON.stringify({
-            id: job.id,
-            name: 'delayed -> ready'
-          }));
+const promote = () => warlock.lock('locks:monitor', 2000, (err, unlock) => {
+  if (err) {
+    // ？？
+    console.log(err);
+  } else if (typeof unlock === 'function') {
+    client.lrange('jobs:delayed', 0, -1, (err, jobs) => {
+      async.each(jobs, (job, callback) => {
+        client.hgetall(`job:${job}`, (err, job) => {
+          if (job.createdAt * 1 + job.delay * 1 > Date.now()) {
+            return callback();
+          }
+
+          client.multi()
+            .hmset(`job:${job}`,
+              'promotedAt', Date.now(),
+              'updatedAt', Date.now(),
+              'state', 'ready'
+            )
+            .lrem('jobs:delayed', 1, job.id)
+            .lrem(`jobs:${job.type}:delayed`, 1, job.id)
+            .lpush('jobs:ready', job.id)
+            .lpush(`jobs:${job.type}:ready`, job.id)
+            .exec(() => {
+              events.emit('promotion', job.id);
+              client.publish(`events:${job.id}`, JSON.stringify({
+                id: job.id,
+                name: 'delayed -> ready'
+              }));
+              callback();
+            });
         });
-      }
+      }, err => {
+        if (err) {
+          console.log(err);
+        } else {
+          unlock();
+        }
+      });
     });
-  });
+  } else {
+    // 我们没拥有锁，则说明不需要我们做什么
+  }
 });
 
-// 每隔一秒查询一次
-setInterval(promote, 1000);
+// 每隔两秒查询一次
+setInterval(promote, 2000);
 
-events.on('promotion', () => {
-  console.log('promote a job');
+events.on('promotion', id => {
+  console.log('promote a job $%d', id);
 });
