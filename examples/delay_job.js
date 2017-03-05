a06e004159e407141024d0e3363db6ae95aa9608
sealday <sealday@gmail.com>
2017-03-06 01:30:55 +0800

    增加延迟工作

--- examples/delay_job.js
+++ examples/delay_job.js
@@ -1,56 +1,76 @@
 'use strict';
-const redis = require('redis');
-const EventEmitter = require('events');
-const Job = require('../lib/job');
-const client = redis.createClient({
-  prefix: 'courier:'
-});
+//const redis = require('redis');
+//const EventEmitter = require('events');
+//const Job = require('../lib/job');
+//const client = redis.createClient({
+//  prefix: 'courier:'
+//});
+//
+//const events = new EventEmitter();
+//const pubsubClient = client.duplicate();
+//
+//pubsubClient.on('message', (channel, message) => {
+//  console.log(channel);
+//  console.log(message);
+//});
+//
+//const run = type => setTimeout(() => {
+//// 新增加的任务总是要创建id
+//  client.incr('id', (err, id) => {
+//    if (err) {
+//      events.emit('error', err);
+//    } else {
+//      events.emit('job ready');
+//      const multi = client.multi();
+//
+//      pubsubClient.subscribe(`events:${id}`);
+//
+//      let job = new Job(id, type, 'delayed');
+//      job.delay = 10 * 1000;
+//      multi.hmset(`job:${id}`, job);
+//
+//      multi.lpush('jobs:delayed', id);
+//      multi.lpush(`jobs:${type}:delayed`, id);
+//      // 实际上上面这个事务也没有什么必要，id 不会被多个客户端
+//      // 这个地方就失败了，只有连接出错的时候才会发生
+//      multi.exec(() => {
+//        client.publish(`events:${id}`, JSON.stringify({
+//          id: id,
+//          name: '-> delayed'
+//        }));
+//      });
+//    }
+//  });
+//  run(type);
+//}, Math.random() * 5000);
+//
+//run('email');
+//run('video');
+//
+//events.on('error', err => {
+//  console.log(err);
+//});
+//
+//events.on('job ready', () => {
+//  console.log('job ready');
+//});
 
-const events = new EventEmitter();
-const pubsubClient = client.duplicate();
-
-pubsubClient.on('message', (channel, message) => {
-  console.log(channel);
-  console.log(message);
-});
-
-const run = type => setTimeout(() => {
-// 新增加的任务总是要创建id
-  client.incr('id', (err, id) => {
-    if (err) {
-      events.emit('error', err);
-    } else {
-      events.emit('job ready');
-      const multi = client.multi();
-
-      pubsubClient.subscribe(`events:${id}`);
-
-      let job = new Job(id, type, 'delayed');
-      job.delay = 10 * 1000;
-      multi.hmset(`job:${id}`, job);
-
-      multi.lpush('jobs:delayed', id);
-      multi.lpush(`jobs:${type}:delayed`, id);
-      // 实际上上面这个事务也没有什么必要，id 不会被多个客户端
-      // 这个地方就失败了，只有连接出错的时候才会发生
-      multi.exec(() => {
-        client.publish(`events:${id}`, JSON.stringify({
-          id: id,
-          name: '-> delayed'
-        }));
-      });
-    }
+setInterval(() => {
+  const courier = require('../');
+  courier.postDelay('email', {
+    title: 'Sample Title',
+    from: 'A',
+    to: 'B',
+    content: 'Some contents'
+  }, 2).on('-> delayed', () => {
+    console.log('进入延迟工作队列');
+  }).on('-> ready', () => {
+    console.log('工作准备就绪');
+  }).on('ready -> active', () => {
+    console.log('工作开始进行');
+  }).on('active -> done', () => {
+    console.log('工作完成');
+  }).on('active -> failed', () => {
+    console.log('工作失败');
   });
-  run(type);
-}, Math.random() * 5000);
-
-run('email');
-run('video');
-
-events.on('error', err => {
-  console.log(err);
-});
-
-events.on('job ready', () => {
-  console.log('job ready');
-});
+}, Math.random() * 2000);
--- examples/monitor_job.js
+++ examples/monitor_job.js
@@ -13,49 +13,6 @@ const client = redis.createClient({
 // 我们应该只有一个监视器
 const warlock = Warlock(client);
 
-const promote = () => warlock.lock('locks:monitor', 2000, (err, unlock) => {
-  if (err) {
-    // ？？
-    console.log(err);
-  } else if (typeof unlock === 'function') {
-    client.lrange('jobs:delayed', 0, -1, (err, jobs) => {
-      async.each(jobs, (job, callback) => {
-        client.hgetall(`job:${job}`, (err, job) => {
-          if (job.createdAt * 1 + job.delay * 1 > Date.now()) {
-            return callback();
-          }
-
-          client.multi()
-            .hmset(`job:${job}`,
-              'promotedAt', Date.now(),
-              'updatedAt', Date.now(),
-              'state', 'ready'
-            )
-            .lrem('jobs:delayed', 1, job.id)
-            .lrem(`jobs:${job.type}:delayed`, 1, job.id)
-            .lpush('jobs:ready', job.id)
-            .lpush(`jobs:${job.type}:ready`, job.id)
-            .exec(() => {
-              events.emit('promotion', job.id);
-              client.publish(`events:${job.id}`, JSON.stringify({
-                id: job.id,
-                name: 'delayed -> ready'
-              }));
-              callback();
-            });
-        });
-      }, err => {
-        if (err) {
-          console.log(err);
-        } else {
-          unlock();
-        }
-      });
-    });
-  } else {
-    // 我们没拥有锁，则说明不需要我们做什么
-  }
-});
 
 // 每隔两秒查询一次
 setInterval(promote, 2000);
