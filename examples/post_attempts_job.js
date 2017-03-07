45900e9ffcdd617289984ca947593dc380f70e5b
sealday <sealday@gmail.com>
2017-03-07 10:40:52 +0800

    失败重试

---
+++ examples/post_attempts_job.js
@@ -0,0 +1,23 @@
+'use strict';
+const courier = require('../').createCourier({ prefix: 'c:' });
+
+/*
+ * 模拟不定期发布工作
+ */
+setInterval(() => {
+  courier.post('email', {
+    title: 'Sample Title',
+    from: 'A',
+    to: 'B',
+    content: 'Some contents'
+  }, { maxAttempts: 5 }).on('-> ready', (job) => {
+    console.log(`#${job.id} 工作准备就绪`);
+  }).on('ready -> active', (job) => {
+    console.log(`#${job.id} 工作开始进行`);
+  }).on('active -> done', (job) => {
+    console.log(`#${job.id} 工作完成`);
+  }).on('active -> failed', (job, message) => {
+    console.log(`#${job.id} 工作失败`);
+    console.log(`失败信息：${message}`);
+  });
+}, Math.random() * 5000);
