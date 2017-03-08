d400832e8ca06ec160a2387cfd6e547214cefd2b
sealday <sealday@gmail.com>
2017-03-08 13:53:41 +0800

    延迟发布工作与发布工作合并

--- examples/delay_job.js
+++ examples/delay_job.js
@@ -5,12 +5,14 @@ const courier = require('../').createCourier({ prefix: 'c:' });
  * 模拟不定期发布延时工作
  */
 setInterval(() => {
-  courier.postDelay('email', {
+  courier.post('email', {
     title: 'Sample Title',
     from: 'A',
     to: 'B',
     content: 'Some contents'
-  }, 2).on(' -> delayed', (job) => {
+  }, {
+    delay: 2,
+  }).on(' -> delayed', (job) => {
     console.log(`#${job.id} 进入延迟工作队列`);
   }).on('delayed -> ready', (job) => {
     console.log(`#${job.id} 进入就绪队列`);
